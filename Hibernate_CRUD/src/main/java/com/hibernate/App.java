6a4107ca61fa84f0d06c795fda5952d12a6a1418
vijaya955 <141250102+vijaya955@users.noreply.github.com>
2025-08-05 07:59:19 +0530

    Add files via upload

---
+++ Hibernate_CRUD/src/main/java/com/hibernate/App.java
@@ -0,0 +1,21 @@
+package com.hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+ 
+public class App {
+ 
+	 public static void main(String[] args)throws Exception {
+		Employee emp = new Employee();
+		emp.setEmpId(102);
+		emp.setEmpname("Jashwanth");
+
+		Configuration con = new Configuration().configure().addAnnotatedClass(Employee.class);
+		SessionFactory sf = con.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction txc =  session.beginTransaction();
+		session.save(emp);
+		txc.commit();
+	}
+}
